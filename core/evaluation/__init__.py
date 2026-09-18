c98ebdaad194f2b24c69f70cd9fe6d0b82b30227
Ahmed EL HOUSNI <housniforbusiness@gmail.com>
2026-09-18 23:52:45 +0100

    B32: add evaluation framework

---
+++ core/evaluation/__init__.py
@@ -0,0 +1,4 @@
+from .models import EvaluationCase, EvaluationResult, EvaluationRun, MetricResult, EvaluationStatus
+from .service import EvaluationFramework, DeterministicEvaluator, EvaluationMetric
+
+__all__=["EvaluationCase","EvaluationResult","EvaluationRun","MetricResult","EvaluationStatus","EvaluationFramework","DeterministicEvaluator","EvaluationMetric"]
